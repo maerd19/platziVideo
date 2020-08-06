088d40e85587bf80d3e58fb3ba5c358194e16560
maerd19 <revolutions_333@hotmail.com>
2020-08-06 12:21:10 -0500

    Implementacion de redux

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import App from './routes/App';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(
+  // Se encapsula nuestra App dentro de un provider, porque nada fuera del provider podrá acceder al store.
+  // El <Provider /> hace que la store de Redux esté disponible para cualquier componente anidado que se haya incluido en la función connect().
+  <Provider>
+    <App />
+  </Provider>,
+  document.getElementById('app')
+);
